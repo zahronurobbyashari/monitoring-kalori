a3767bab0bde7caac9cb64d470dbc945317c8bdc
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-08-25 22:21:33 +0700

    [FEAT] circulator percent kkal indicator

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:monitoring_kalori/app/data/theme/appTheme.dart';
 import 'package:monitoring_kalori/app/modules/commons/navigation_drawer/views/navigation_drawer_view.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -28,9 +29,16 @@ class HomeView extends GetView<HomeController> {
         ),
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: CircularPercentIndicator(
+          radius: 150,
+          animation: true,
+          circularStrokeCap: CircularStrokeCap.round,
+          progressColor: appThemeData.accentColor,
+          percent: 0.7,
+          center: Text(
+            "70.0%",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
         ),
       ),
     );

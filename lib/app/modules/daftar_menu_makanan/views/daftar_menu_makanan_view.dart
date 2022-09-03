ef40f0edf7f320c76fe234db780af58449f2c9a2
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-09-03 22:36:28 +0700

    [IMPROVE] change multiplier to per 100 gram

--- lib/app/modules/daftar_menu_makanan/views/daftar_menu_makanan_view.dart
+++ lib/app/modules/daftar_menu_makanan/views/daftar_menu_makanan_view.dart
@@ -48,7 +48,11 @@ class DaftarMenuMakananView extends GetView<DaftarMenuMakananController> {
                   title: Text(
                       "${(listFoods[index].data() as Map<String, dynamic>)["food_name"]}"),
                   subtitle: Text(
-                    "kalori : ${listFoods[index].get('multiplier')} per 1 gram",
+                    // ignore: prefer_interpolation_to_compose_strings
+                    "kalori : " +
+                        (double.parse(listFoods[index].get('multiplier')) * 100)
+                            .toString() +
+                        " per 100 gram",
                   ),
                 ),
               );
